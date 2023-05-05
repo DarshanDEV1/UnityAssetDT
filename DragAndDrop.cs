78b03fef05257306ab2b7084de5b95a36280c90f
Bedaprakash Saidarshan Sahoo <86543754+DarshanDEV1@users.noreply.github.com>
2023-05-05 15:57:32 +0530

    Add files via upload

---
+++ DragAndDrop.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    private void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    private void OnMouseUp()
+    {
+        // Perform actions on drop
+    }
+}
